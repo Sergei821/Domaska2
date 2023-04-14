f361762f7d1e07db3b653ada091491f095e7de28
Sergei821 <ps16111982@gmail.com>
2023-04-15 00:36:33 +0300

    zad1

--- Zad1/Program.cs
+++ Zad1/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿//Задача 1: Напишите программу, которая принимает на вход трёхзначное число и 
+//на выходе показывает вторую цифру этого числа. Не использовать строки для расчета 
+//(решать только при помощи операций % и /).
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int number = Prompt("введите трёхзначное число > ");
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("вы ввели не трёхзначное число, пожалуйста повторите ввод");
+    return;
+}
+Console.WriteLine($"введите число '{number}'");
+int secondRank = number /10 %10;
+Console.WriteLine($"вторая цифра '{secondRank}'");
